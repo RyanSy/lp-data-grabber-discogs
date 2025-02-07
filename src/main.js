6ed14d2a2008b3857d4ce665a3673c29989cf7ed
Ryan Sy <ryanbsy@gmail.com>
2025-02-06 20:04:59 -0500

    search discogs/write to csv working

---
+++ src/main.js
@@ -0,0 +1,88 @@
+const search = require('./search');
+const titles = require('./inventory');
+const writeCSV = require('write-csv');
+const titlesLength = titles.length;
+const products = [];
+const date = new Date().toISOString();
+
+async function main() {
+// perform 60 searches at a time, write data to csv, then delete those elements from the array
+    for (let i = 0; i < 5; i++) {
+        const album = await search.searchDiscogs(titles[i]);
+        const year = album.year;
+        const format = album.format;
+        const label = album.label;
+        const genre = album.genre;
+        const style = album.style;
+        const title = album.title;
+        const cover_image = album.cover_image;
+        const product = {
+            'Handle': title,
+            'Title': title,
+            'Body (HTML)': '',
+            'Vendor': 'Village Record Club',
+            'Product Category': 'Media > Music & Sound Recordings > Records & LPs',
+            'Type': 'album',
+            'Tags': `${genre}, ${style}, ${label}, ${year}`,
+            'Published': '',
+            'Option1 Name': '',
+            'Option1 Value': '',
+            'Option2 Name': '',
+            'Option2 Value': '',
+            'Option3 Name': '',
+            'Option3 Value': '',
+            'Variant SKU': '',
+            'Variant Grams': '',
+            'Variant Inventory Tracker': '',
+            'Variant Inventory Qty': '',
+            'Variant Inventory Policy': 'deny',
+            'Variant Fulfillment Service': 'manual',
+            'Variant Price': '',
+            'Variant Compare At Price': '',
+            'Variant Requires Shipping': '',
+            'Variant Taxable': '',
+            'Variant Barcode': '',
+            'Image Src': cover_image,
+            'Image Position': '',
+            'Image Alt Text': '',
+            'Gift Card': '',
+            'SEO Title': '',
+            'SEO Description': '',
+            'Google Shopping / Google Product Category': '',
+            'Google Shopping / Gender': '',
+            'Google Shopping / Age Group': '',
+            'Google Shopping / MPN': '',
+            'Google Shopping / AdWords Grouping': '',
+            'Google Shopping / AdWords Labels': '',
+            'Google Shopping / Condition': '',
+            'Google Shopping / Custom Product': '',
+            'Google Shopping / Custom Label 0': '',
+            'Google Shopping / Custom Label 1': '',
+            'Google Shopping / Custom Label 2': '',
+            'Google Shopping / Custom Label 3': '',
+            'Google Shopping / Custom Label 4': '',
+            'Variant Image': '',
+            'Variant Weight Unit': '',
+            'Variant Tax Code': '',
+            'Cost per item': '',
+            'Price / International': '',
+            'Compare At Price / International': '',
+            'Status': 'Active'
+        };
+
+        products.push(product);
+        try {
+            console.log('Writing to .csv file...');
+            writeCSV(`./csv/${date}.csv`, products);
+        } catch(err) {
+            console.error('Error writing to .csv file:', err);
+        }
+        
+        // await titles.splice(0, 60)
+
+    }
+}
+
+// 
+
+main();
---
+++ src/search.js
@@ -0,0 +1,63 @@
+require('dotenv').config();
+
+const url = 'https://api.discogs.com';
+const consumerKey = process.env.DISCOGS_CONSUMER_KEY;
+const consumerSecret = process.env.DISCOGS_CONSUMER_SECRET;
+const options = {
+    headers: {
+        'User-Agent': 'LP Data Grabber/1.0'
+    }
+}
+
+/**
+ * Searches Discogs database for album info
+ * @param {String} query - The album to search for
+ * @returns {Object} A JSON object containing album info
+ * 
+ * sample output:
+ * 
+ * {
+  country: 'US',
+  year: '1994',
+  format: [ 'Vinyl', 'LP', 'Album' ],
+  label: [ 'Columbia', 'Sony Music Entertainment Inc.' ],
+  type: 'master',
+  genre: [ 'Hip Hop' ],
+  style: [ 'Conscious', 'Boom Bap' ],
+  id: 20148,
+  barcode: [
+    '0 7464-57684-1 0',
+    'AL 57684',
+    'BL 57684',
+    'AL-57684-1A 2-1 MASTERDISK TD',
+    'BL-57684-1A 1-1'
+  ],
+  master_id: 20148,
+  master_url: 'https://api.discogs.com/masters/20148',
+  uri: '/master/20148-Nas-Illmatic',
+  catno: 'C 57684',
+  title: 'Nas - Illmatic',
+  thumb: 'https://i.discogs.com/1GrA9UIHGuydnr9LlBMIP0lKySfmeU9-apaiVvbImmc/rs:fit/g:sm/q:40/h:150/w:150/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTM5MjYw/NC0xNjk2MDg5NjAw/LTY2MTcuanBlZw.jpeg',
+  cover_image: 'https://i.discogs.com/wrbuutLseZDQSdcRIKF9dK1k-K00qs08bHHqr-bZ0p8/rs:fit/g:sm/q:90/h:593/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTM5MjYw/NC0xNjk2MDg5NjAw/LTY2MTcuanBlZw.jpeg',
+  resource_url: 'https://api.discogs.com/masters/20148',
+  community: { want: 81193, have: 107419 }
+}
+ */
+async function searchDiscogs(query) {
+    console.log('Searching Discogs database...');
+
+    const album = await fetch(`${url}/database/search?q=${query}&type=album&key=${consumerKey}&secret=${consumerSecret}`, options)
+        .then(result => {
+            console.log('Album Data Found!');
+            return result.json();
+        })
+        .then(body => {
+            const albumData = body.results[0];
+            return albumData;
+        })
+        .catch(err => console.error('Error searching Discogs database:', err));
+
+    return album;   
+}
+
+module.exports = { searchDiscogs };
