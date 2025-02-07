464905d3f5621a03ef2191e67d8d5c31faf67303
Ryan Sy <ryanbsy@gmail.com>
2025-02-07 02:47:21 -0500

    bug fix

--- src/main.js
+++ src/main.js
@@ -3,73 +3,80 @@ const titles = require('./inventory');
 const writeCSV = require('write-csv');
 const titlesLength = titles.length;
 const products = [];
+const rejects = [];
 
 async function main() {
     // perform 60 searches at a time, write data to csv, then delete those elements from the array
     for (let i = 0; i < 60; i++) {
         const album = await search.searchDiscogs(titles[i]);
-        const format = album.format.join();
-        const label = album.label.join();
-        const genre = album.genre.join();
-        const style = album.style.join();
-        const title = album.title;
-        const cover_image = album.cover_image;
-        const product = {
-            'Handle': title,
-            'Title': title,
-            'Body (HTML)': '',
-            'Vendor': 'Village Record Club',
-            'Product Category': 'Media > Music & Sound Recordings > Records & LPs',
-            'Type': 'album',
-            'Tags': `${format}, ${label}, ${genre}, ${style}`,
-            'Published': '',
-            'Option1 Name': '',
-            'Option1 Value': '',
-            'Option2 Name': '',
-            'Option2 Value': '',
-            'Option3 Name': '',
-            'Option3 Value': '',
-            'Variant SKU': '',
-            'Variant Grams': '',
-            'Variant Inventory Tracker': '',
-            'Variant Inventory Qty': '',
-            'Variant Inventory Policy': 'deny',
-            'Variant Fulfillment Service': 'manual',
-            'Variant Price': '',
-            'Variant Compare At Price': '',
-            'Variant Requires Shipping': '',
-            'Variant Taxable': '',
-            'Variant Barcode': '',
-            'Image Src': cover_image,
-            'Image Position': '',
-            'Image Alt Text': title,
-            'Gift Card': '',
-            'SEO Title': `${title} | Village Record Club`,
-            'SEO Description': `${title} is available to add to your wishlist.`,
-            'Google Shopping / Google Product Category': '',
-            'Google Shopping / Gender': '',
-            'Google Shopping / Age Group': '',
-            'Google Shopping / MPN': '',
-            'Google Shopping / AdWords Grouping': '',
-            'Google Shopping / AdWords Labels': '',
-            'Google Shopping / Condition': '',
-            'Google Shopping / Custom Product': '',
-            'Google Shopping / Custom Label 0': '',
-            'Google Shopping / Custom Label 1': '',
-            'Google Shopping / Custom Label 2': '',
-            'Google Shopping / Custom Label 3': '',
-            'Google Shopping / Custom Label 4': '',
-            'Variant Image': '',
-            'Variant Weight Unit': '',
-            'Variant Tax Code': '',
-            'Cost per item': '',
-            'Price / International': '',
-            'Compare At Price / International': '',
-            'Status': 'Active'
-        };
+        
+        if (typeof album !== 'undefined') {
+            const format = album.format.join();
+            const label = album.label.join();
+            const genre = album.genre.join();
+            const style = album.style.join();
+            const title = album.title;
+            const cover_image = album.cover_image;
+            const product = {
+                'Handle': title,
+                'Title': title,
+                'Body (HTML)': '',
+                'Vendor': 'Village Record Club',
+                'Product Category': 'Media > Music & Sound Recordings > Records & LPs',
+                'Type': 'album',
+                'Tags': `${format}, ${label}, ${genre}, ${style}`,
+                'Published': '',
+                'Option1 Name': '',
+                'Option1 Value': '',
+                'Option2 Name': '',
+                'Option2 Value': '',
+                'Option3 Name': '',
+                'Option3 Value': '',
+                'Variant SKU': '',
+                'Variant Grams': '',
+                'Variant Inventory Tracker': '',
+                'Variant Inventory Qty': '',
+                'Variant Inventory Policy': 'deny',
+                'Variant Fulfillment Service': 'manual',
+                'Variant Price': '',
+                'Variant Compare At Price': '',
+                'Variant Requires Shipping': '',
+                'Variant Taxable': '',
+                'Variant Barcode': '',
+                'Image Src': cover_image,
+                'Image Position': '',
+                'Image Alt Text': title,
+                'Gift Card': '',
+                'SEO Title': `${title} | Village Record Club`,
+                'SEO Description': `${title} is available to add to your wishlist.`,
+                'Google Shopping / Google Product Category': '',
+                'Google Shopping / Gender': '',
+                'Google Shopping / Age Group': '',
+                'Google Shopping / MPN': '',
+                'Google Shopping / AdWords Grouping': '',
+                'Google Shopping / AdWords Labels': '',
+                'Google Shopping / Condition': '',
+                'Google Shopping / Custom Product': '',
+                'Google Shopping / Custom Label 0': '',
+                'Google Shopping / Custom Label 1': '',
+                'Google Shopping / Custom Label 2': '',
+                'Google Shopping / Custom Label 3': '',
+                'Google Shopping / Custom Label 4': '',
+                'Variant Image': '',
+                'Variant Weight Unit': '',
+                'Variant Tax Code': '',
+                'Cost per item': '',
+                'Price / International': '',
+                'Compare At Price / International': '',
+                'Status': 'Active'
+            };
 
         await products.push(product);
 
+        } else {
+            await rejects.push(titles[i]);
+        }
+        
         await titles.splice(0, 60);
     }
 
