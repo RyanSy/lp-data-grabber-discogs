39751e6d6f81af28f0be2ea4d34b370dc636bb23
Ryan Sy <ryanbsy@gmail.com>
2025-02-07 22:37:40 -0500

    working

---
+++ src/albumTitles.js
@@ -0,0 +1,3 @@
+const albumTitles = ['gang starr = hard to earn', 'conway - calvin', 'jeru = wrath of math'];
+
+module.exports = albumTitles;
--- src/search.js
+++ src/search.js
@@ -5,65 +5,48 @@ const consumerKey = process.env.DISCOGS_CONSUMER_KEY;
 const consumerSecret = process.env.DISCOGS_CONSUMER_SECRET;
 const options = {
     headers: {
-        'User-Agent': 'LP Data Grabber/1.0'
+        'User-Agent': 'Record Show Mania/1.0 (Contact: recordshowmania@gmail.com)'
     }
 }
+const type = 'release';
+const format ='vinyl';
 
 /**
  * Searches Discogs database for album info
  * @param {String} query - The album to search for
  * @returns {Object} A JSON object containing album info
- * 
- * sample output:
- * 
- * {
-  country: 'US',
-  year: '1994',
-  format: [ 'Vinyl', 'LP', 'Album' ],
-  label: [ 'Columbia', 'Sony Music Entertainment Inc.' ],
-  type: 'master',
-  genre: [ 'Hip Hop' ],
-  style: [ 'Conscious', 'Boom Bap' ],
-  id: 20148,
-  barcode: [
-    '0 7464-57684-1 0',
-    'AL 57684',
-    'BL 57684',
-    'AL-57684-1A 2-1 MASTERDISK TD',
-    'BL-57684-1A 1-1'
-  ],
-  master_id: 20148,
-  master_url: 'https://api.discogs.com/masters/20148',
-  uri: '/master/20148-Nas-Illmatic',
-  catno: 'C 57684',
-  title: 'Nas - Illmatic',
-  thumb: 'https://i.discogs.com/1GrA9UIHGuydnr9LlBMIP0lKySfmeU9-apaiVvbImmc/rs:fit/g:sm/q:40/h:150/w:150/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTM5MjYw/NC0xNjk2MDg5NjAw/LTY2MTcuanBlZw.jpeg',
-  cover_image: 'https://i.discogs.com/wrbuutLseZDQSdcRIKF9dK1k-K00qs08bHHqr-bZ0p8/rs:fit/g:sm/q:90/h:593/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTM5MjYw/NC0xNjk2MDg5NjAw/LTY2MTcuanBlZw.jpeg',
-  resource_url: 'https://api.discogs.com/masters/20148',
-  community: { want: 81193, have: 107419 }
-}
  */
 async function searchDiscogs(query) {
-    if (typeof query !== 'undefined') {
-        console.log(`Searching Discogs database for ${query}...`);
+    console.log(`Searching Discogs database for ${query}...`);
 
-        const album = await fetch(`${url}/database/search?q=${query}&type=release&format=vinyl&key=${consumerKey}&secret=${consumerSecret}`, options)
-            .then(result => {
-                return result.json();
-            })
-            .then(body => {
+    let discogsResponseHeaders = {}
+
+    const album = await fetch(`${url}/database/search?q=${query}&type=${type}&format=${format}&key=${consumerKey}&secret=${consumerSecret}`, options)
+        .then(async response => {
+            const responseHeaders = response.headers;
+            discogsResponseHeaders['x-discogs-ratelimit'] = await responseHeaders.get('x-discogs-ratelimit');
+            discogsResponseHeaders['x-discogs-ratelimit-remaining'] = await responseHeaders.get('x-discogs-ratelimit-remaining');
+            discogsResponseHeaders['x-discogs-ratelimit-used'] = await responseHeaders.get('x-discogs-ratelimit-used');
+            console.log(discogsResponseHeaders);
+            return response.json();
+        })
+        .then(body => {
+            if (body.results.length > 0) {
+                console.log('Album data found.');
                 const albumData = body.results[0];
-                console.log(`Album data found for ${albumData.title}`);
-                // console.log(albumData)
                 return albumData;
-            })
-            .catch(err => console.error('Error searching Discogs database:', err));
-        
-        return album; 
+            } else {
+                console.log('No album data found.');
+                return null;
+            }
+        })
+        .catch(err => console.error('Error searching Discogs database:', err));
+    
+    if (typeof album !== 'undefined') {
+        return album;
     } else {
-        console.log('Search query returns undefined...')
+        return null;
     }
-      
 }
 
 module.exports = { searchDiscogs };
